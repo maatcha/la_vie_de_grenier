a5f8136f14c7537948a55c912664c9ed83b10586
maatcha <mattcha54@gmail.com>
2020-08-05 19:48:33 +0200

    Revert "no server redirection"
    This reverts commit 0890bc590a61d126a069cb02f53a03d030d8b388.

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 const express = require('express')
 const port = process.env.PORT || 8080
-const env = process.env.NODE_ENV || 'development'
 const app = express()
 
 app.use(express.static(__dirname + '/dist/'))
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + '/dist/index.html')
 })
-
-var forceSsl = function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''))
-  }
-  return next()
-}
-
-app.configure(function() {
-  if (env === 'production') {
-    app.use(forceSsl)
-  }
-})
-
 app.listen(port)
 
 console.log(`App is listening on port: ${port}`)
